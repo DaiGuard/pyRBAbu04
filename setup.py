a331c94591da957eb5784e0f21bbe6edd21998be
DaiGuard <zenith.or.w@gmail.com>
2022-06-12 20:57:05 +0900

    change

--- setup.py
+++ setup.py
@@ -23,6 +23,6 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ]    
+    ],
     python_requires='>=3.9',
 )
